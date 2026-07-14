0e4c9cea2b943d7c79ea6b73fa55ecffdc96f0a8
sslav <sslavaa2212@gmail.com>
2026-07-14 14:11:53 +1000

    Add import

--- src/main/java/sk/thefogiof/hwextra/network/FeatureControlPayload.java
+++ src/main/java/sk/thefogiof/hwextra/network/FeatureControlPayload.java
@@ -1,7 +1,6 @@
 package sk.thefogiof.hwextra.network;
 
 import io.netty.buffer.ByteBuf;
-import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
